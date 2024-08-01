767801169fa8a57f62b187c5e3e4c874b7fc477e
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-08-01 11:56:45 +0530

    Update ArtistRepository.java

--- src/main/java/com/example/artgallery/repository/ArtistRepository.java
+++ src/main/java/com/example/artgallery/repository/ArtistRepository.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface ArtistRepository {
-    ArrayList<Artist getArtists();
+    ArrayList<Artist> getArtists();
 
     Artist getArtistById(int artistId);
 
