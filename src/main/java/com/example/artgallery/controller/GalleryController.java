5cda0fdf32b5683078002fc764e0a6e6e00398c4
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-08-02 13:27:08 +0530

    Update GalleryController.java

--- src/main/java/com/example/artgallery/controller/GalleryController.java
+++ src/main/java/com/example/artgallery/controller/GalleryController.java
@@ -4,7 +4,7 @@ import com.example.artgallery.model.Artist;
 import com.example.artgallery.model.Gallery;
 import com.example.artgallery.service.GalleryJpaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
