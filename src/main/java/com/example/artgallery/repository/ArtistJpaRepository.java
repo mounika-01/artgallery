e496325afc9a3f3bcfa49e7f7b1d861ae3aefd07
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-08-02 13:31:43 +0530

    Update ArtistJpaRepository.java

--- src/main/java/com/example/artgallery/repository/ArtistJpaRepository.java
+++ src/main/java/com/example/artgallery/repository/ArtistJpaRepository.java
@@ -1,11 +1,13 @@
 package com.example.artgallery.repository;
 
+import com.example.artgallery.model.Art;
 import com.example.artgallery.model.Artist;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
 
+import java.util.*;
+
 @Repository
-public interface ArtistJpaRepository extends JpaRepository<Artist, Integer> {
+public interface ArtJpaRepository extends JpaRepository<Art, Integer> {
+    List<Art> findByArtist(Artist artist);
 }
